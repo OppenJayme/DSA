a836110542eba268d0bdc0e1491678f40929bc37
Kyer Ivan <156319128+OppenJayme@users.noreply.github.com>
2025-06-18 15:36:01 +0800

    add: reversal array

--- premid-term/Arrays/version-one/pracitce.c
+++ premid-term/Arrays/version-one/pracitce.c
@@ -1,3 +1,4 @@
+// Online C compiler to run C program online
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -9,13 +10,19 @@ typedef struct {
 } LIST;
 
 bool locate(LIST l, char c);
+void reversal(LIST *l);
 
 int main() {
-    LIST l = {"USC", 3};
+    LIST l = {"gwapo", 5};
     
-    bool locator = locate(l,'C');
-    printf("%s", (locator == 0) ? "False" : "True");
-
+    bool locator = locate(l,'D');
+    printf("%s\n", (locator == 0) ? "False" : "True");
+    
+    reversal(&l);
+    for (int i = 0; i < l.elemCount;++i) {
+        printf("%c", l.elem[i]);
+    }
+    
     return 0;
 }
 
@@ -26,4 +33,18 @@ bool locate(LIST l, char c){
         printf("Letter Currently at %c and i = %d\n", l.elem[i], i);
     }
     return (i < l.elemCount) ? true : false; // if i < than l.elemCount then char c is equal to l.elem[i]. if its not lower than elemCount
+}
+
+void reversal(LIST *l){
+    int i;
+    int j = 0;
+    int median = l->elemCount / 2; 
+        for (i = l->elemCount-1; i > 0;  --i){ 
+            if (i - j != 1 || (i != median && j != median)){
+                char temp = l->elem[i];
+                l->elem[i] = l->elem[j];
+                l->elem[j] = temp;
+            }
+            j++;
+        }
 }
