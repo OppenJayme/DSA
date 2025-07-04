9ed6143305bc6c824849de257b6c2465a1749987
OppenJayme <jaymekyerivan@gmail.com>
2025-07-04 15:10:34 +0800

    add: more functions

--- premid-term/Cursor-Based/basics.c
+++ premid-term/Cursor-Based/basics.c
@@ -1,6 +1,7 @@
 // Given VirtualHeap and LIST
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #define MAX 7
 
 typedef struct {
@@ -15,7 +16,14 @@ typedef struct {
 typedef int LIST;
 
 void initList(LIST *VH);
+void read(Vheap H, LIST L);
+void initHeap(Vheap *L);
+void freeVH(Vheap *H, int idx);
 void insertFirst(Vheap *H, LIST *L, char data);
+void insertLast(Vheap *H, LIST *L, char data);
+
+void delete(Vheap *H, LIST *L, char data);
+bool locate(Vheap H, LIST L, char data);
 
 int main(){
     LIST lista;
@@ -25,7 +33,21 @@ int main(){
     initHeap(&heap);
 
     insertFirst(&heap, &lista, 'U');
+    read(heap,lista);
+    
+    insertFirst(&heap, &lista, 'C');
+    read(heap,lista);
+    insertLast(&heap, &lista, 'S');
+    read(heap,lista);
+    delete(&heap, &lista, 'C');
+    read(heap,lista);
 
+    printf("%d", locate(heap, lista, 'U'));
+   
+    
+   
+    
+    
 
     return 0;
 }
@@ -36,10 +58,11 @@ void initList(LIST *L){
 
 void initHeap(Vheap *L){
     L->avail = 0;
-    L->Nodes[MAX - 1].link = -1;
-    for (int i = 1; i < MAX - 1; ++i){
+    
+    for (int i = 1; i < MAX; ++i){
         L->Nodes[i - 1].link = i;
     }
+    L->Nodes[MAX - 1].link = -1;
 }
 
 int allocSpace(Vheap *H){
@@ -49,16 +72,58 @@ int allocSpace(Vheap *H){
     }
     return idx; 
 }
+void freeVH(Vheap *H, int idx){
+    if (idx >= 0 && idx < MAX){
+        H->Nodes[idx].link = H->avail; // at the index of where link is, assign link to h->avail for the index to be flagged available;
+        H->avail = idx; // assign h->avail to ihe index of the newly allocated space. meaning there will be an available space in that index.
+    }
+}
+void read(Vheap H, LIST L) {
+    printf("CB List: ");
+
+    for(int i = L; i != -1; i = H.Nodes[i].link) {
+        printf("%c%s", H.Nodes[i].data, (H.Nodes[i].link == -1) ? ".\n" : ", ");
+    }
+
+    if(L == -1) {
+        printf("EMPTY\n");
+    }
+}
 
 void insertFirst(Vheap *H, LIST *L, char data){
     int newNode = allocSpace(H);
     if (newNode != -1){
-        H->Nodes[newNode].data = data;
-        H->Nodes[newNode].link = *L;    
-        printf("%d", *L);
+        H->Nodes[newNode].data =  data;
+        H->Nodes[newNode].link = *L;
         *L = newNode;
     }
     
 }
 
+void insertLast(Vheap *H, LIST *L, char data){
+    int newNode = allocSpace(H);
+    if (newNode != -1){
+        int *curr;
+        for (curr = L; *curr != -1; curr = &(H->Nodes[*curr].link)){}
+        H->Nodes[newNode].data = data;
+        H->Nodes[newNode].link = -1;
+        *curr = newNode;
+}
+}
 
+void delete(Vheap *H, LIST *L, char data){
+    int *curr;
+    for (curr = L; *curr != -1 && H->Nodes[*curr].data != data; curr = &H->Nodes[*curr].link){}
+    if (*curr != -1){
+        int temp = *curr;
+        *curr = H->Nodes[*curr].link;
+        freeVH(H, temp);
+    }
+}
+
+bool locate(Vheap H, LIST L, char data){
+    int curr;
+    for (curr = L; curr != -1 && H.Nodes[curr].data != data; curr = H.Nodes[curr].link){}
+    return  (curr != -1) ? true : false;
+}
+    
